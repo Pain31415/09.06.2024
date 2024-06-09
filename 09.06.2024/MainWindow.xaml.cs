9e236813a2fc10a7c8a4aa372e7e25d5214e5fcf
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-06-09 12:17:51 +0300

    Add project files.

---
+++ 09.06.2024/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _09._06._2024
+{
+    public partial class MainWindow : Window
+    {
+        private Mutex mutex = new Mutex();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            OutputTextBox.Clear();
+            Task.Run(() => FirstThread());
+        }
+
+        private void FirstThread()
+        {
+            mutex.WaitOne();
+            for (int i = 0; i <= 20; i++)
+            {
+                Dispatcher.Invoke(() => OutputTextBox.AppendText(i + "\n"));
+                Thread.Sleep(2000);
+            }
+            mutex.ReleaseMutex();
+            Task.Run(() => SecondThread());
+        }
+
+        private void SecondThread()
+        {
+            mutex.WaitOne();
+            for (int i = 10; i >= 0; i--)
+            {
+                Dispatcher.Invoke(() => OutputTextBox.AppendText(i + "\n"));
+                Thread.Sleep(2000);
+            }
+            mutex.ReleaseMutex();
+        }
+    }
+}
